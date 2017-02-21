a0bb3ddf5c4ecdcf20eef4d036a133b20aa569a7
Sam <thesamreaves@gmail.com>
2017-02-20 22:06:22 -0500

    Added sample reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+/**
+ * Redux Example in React Native
+ * File System Import Helper for Reducers
+ * Written by Sam Reaves
+ * February 20, 2017
+ * https://github.com/samreaves/learn-react-native-redux
+ */
+
+ import { combineReducers } from 'redux';
+
+ export default combineReducers({
+   libraries: () => []
+ });
