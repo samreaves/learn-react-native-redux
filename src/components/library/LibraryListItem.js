28276e34bb17618ab72355074e8aa3ba3fbec45e
Sam <thesamreaves@gmail.com>
2017-02-23 22:09:42 -0500

    Updated LibraryListItem with button, select library action, redux connection, and library description render only if app's selected id matches library id

--- src/components/library/LibraryListItem.js
+++ src/components/library/LibraryListItem.js
@@ -7,26 +7,54 @@
  */
 
  import React, { Component } from 'react';
- import { Text } from 'react-native';
+ import { connect } from 'react-redux';
+ import { Text, View, TouchableWithoutFeedback } from 'react-native';
  import { CardSection } from '../common';
+ import * as actions from '../../actions';
 
  class LibraryListItem extends Component {
 
+   /* Render expanded description */
+   renderDescription() {
+
+     const { library, selectedLibraryID } = this.props;
+
+     /* If library list item's id is app's current selected library id  */
+     if (library.id === selectedLibraryID) {
+       return (
+         <Text>
+           {this.props.library.description}
+         </Text>
+       );
+     }
+   }
+
+   /* Render entire Library List Item */
    render() {
 
      const styles = {
        fontSize: 18,
-       paddingLeft: 15
      };
 
+     const { id, title } = this.props.library;
+
      return (
-         <CardSection>
-           <Text style={styles}>
-             {this.props.library.title}
-           </Text>
-         </CardSection>
+       <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+         <View>
+           <CardSection>
+             <Text style={styles}>
+               {title}
+             </Text>
+             {this.renderDescription()}
+           </CardSection>
+         </View>
+      </TouchableWithoutFeedback>
      );
    }
  }
 
- export default LibraryListItem;
+ const mapStateToProps = state => {
+   return { selectedLibraryID: state.selectedLibraryID };
+ };
+
+ export default connect(mapStateToProps, actions)(LibraryListItem);
