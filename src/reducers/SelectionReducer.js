f9075fa5a4db89d5794680330c3f81e12778f919
Sam <thesamreaves@gmail.com>
2017-02-23 22:07:07 -0500

    Added true selection reducer and updated its property reference.

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -2,7 +2,15 @@
  * Redux Example in React Native
  * Selection Reducer
  * Written by Sam Reaves
- * February 20, 2017
+ * February 23, 2017
  * https://github.com/samreaves/learn-react-native-redux
  */
-export default () => 1;
+export default (state = null, action) => {
+  switch (action.type) {
+    case 'select_library':
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,5 +12,5 @@
 
  export default combineReducers({
    libraries: LibraryReducer,
-   selection: SelectionReducer
+   selectedLibraryID: SelectionReducer
  });
